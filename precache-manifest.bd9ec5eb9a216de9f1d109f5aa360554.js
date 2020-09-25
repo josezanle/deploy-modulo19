b00bf354a2f9b45f9b2286a47ba71c2d973c8d17
josezanle <63666176+josezanle@users.noreply.github.com>
2020-09-25 20:14:28 -0300

    este es el reositorio para hacer deploy la aplicacion

---
+++ precache-manifest.bd9ec5eb9a216de9f1d109f5aa360554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98908427955821f318997c48c2ab5b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45fab8a51f715e9660d5",
+    "url": "/static/css/main.dc640bcf.chunk.css"
+  },
+  {
+    "revision": "b12d8a6b00eea5c28c3d",
+    "url": "/static/js/2.b80990de.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.b80990de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45fab8a51f715e9660d5",
+    "url": "/static/js/main.3cf95715.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
